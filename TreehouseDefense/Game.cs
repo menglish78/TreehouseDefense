bc1bda63e4b443a064582977afa4d85b4bcbcaf1
Matt English <menglish78@bellsouth.net>
2016-09-10 14:53:29 -0400

    Worked on inheritance, throwing and catching exceptions, and started on arrays and encapsulation. Added Exception.cs and MapLocation.cs classes.

--- TreehouseDefense/Game.cs
+++ TreehouseDefense/Game.cs
@@ -12,9 +12,33 @@ namespace TreehouseDefense
         {
             Map map = new Map(8, 5);
 
-            Point point = new Point(4, 2);
+            try
+            {
+                MapLocation[] path = {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map)
+                };
+            }
+            catch (OutOfBoundsException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (TreehouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unhandled Exception");
+            }
 
-            Console.WriteLine(point.DistanceTo(5, 5));
+            Console.ReadLine();
         }
     }
 }
--- TreehouseDefense/Path.cs
+++ TreehouseDefense/Path.cs
@@ -8,5 +8,11 @@ namespace TreehouseDefense
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
     }
 }
